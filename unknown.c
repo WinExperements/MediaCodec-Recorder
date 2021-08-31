bf79ab7365b876d3630c25984b0a325a3d598f45
Sergij Yevchuk <85248187+WinExperements@users.noreply.github.com>
2021-08-31 13:03:58 +0300

    Create unknown.c

---
+++ unknown.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+       //You can download the source code of app in release.
+       printf("YOU CAN DOWNLOAD THE SOURCE CODE OF THE APP IN RELEASE PAGE OF GITHUB\n");
+       while(1) {}
+       return 0;
+}
